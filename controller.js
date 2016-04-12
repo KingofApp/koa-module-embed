4636dceea5b4104eb13151ff99f59a3c8dda1d14
Fernando Lafuente saiz <fernandolafuentesaiz@gmail.com>
2016-04-12 17:29:02 +0200

    Support for embedlogin service

--- controller.js
+++ controller.js
@@ -1,10 +1,54 @@
 angular
   .controller('embedCtrl', loadFunction);
 
-loadFunction.$inject = ['$scope', 'structureService', '$location'];
+loadFunction.$inject = ['$scope', 'structureService', 'storageService', '$location'];
 
-function loadFunction($scope, structureService, $location){
+function loadFunction($scope, structureService, storageService, $location) {
   //Register upper level modules
-  structureService.registerModule($location,$scope,"embed");
+  structureService.registerModule($location, $scope, 'embed');
 
+  $scope.embed.showLogin = false;
+
+  storageService.get('embedLogin').then(function(data) {
+    if (data && data.value) {
+
+      post($scope.embed.modulescope.url, data.value);
+      $scope.embed.showLogin = true;
+    }
+
+  });
+  $scope.redirectToLogin = function() {
+    console.log("TEST");
+    //PETARSE el embedLogin
+    storageService.del('embedLogin').then(function(data) {
+      console.log("DATA",data);
+      storageService.get('loginUrl').then(function(src) {
+        if(src && src.value){
+          $location.path(src.value);
+        }
+      });
+    });
+  }
+
+  function post(path, params, method) {
+    method = method || 'post';
+
+    var form = document.createElement('form');
+    form.setAttribute('method', method);
+    form.setAttribute('action', path);
+    form.setAttribute('target', 'embedtest');
+
+    for (var key in params) {
+      if (params.hasOwnProperty(key)) {
+        var hiddenField = document.createElement('input');
+        hiddenField.setAttribute('type', 'hidden');
+        hiddenField.setAttribute('name', key);
+        hiddenField.setAttribute('value', params[key]);
+
+        form.appendChild(hiddenField);
+      }
+    }
+    document.body.appendChild(form);
+    form.submit();
+  }
 }
