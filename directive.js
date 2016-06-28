ac0aef83f80cf6c6b0284cbd8489354db4ce9984
Fernando Lafuente saiz <fernandolafuentesaiz@gmail.com>
2016-06-28 17:28:29 +0200

    Directive code inside function

--- directive.js
+++ directive.js
@@ -1,14 +1,16 @@
-angular.directive('hideOnload', [function(){
-return {
-    scope: {
-        callBack: '&hideOnload'
-    },
-    link: function(scope, element, attrs){
-        element.on('load', function(){
-            scope.$parent.iframe.showLoading=false;
-            element.parent().find("div.iframeLoading").hide();
-            iFrameResize({log:true});
-            return scope.callBack();
-        })
-    }
-}}]);
+(function() {
+  angular.directive('hideOnload', [function(){
+  return {
+      scope: {
+          callBack: '&hideOnload'
+      },
+      link: function(scope, element, attrs){
+          element.on('load', function(){
+              scope.$parent.iframe.showLoading=false;
+              element.parent().find("div.iframeLoading").hide();
+              iFrameResize({log:true});
+              return scope.callBack();
+          })
+      }
+  }}]);
+}());
