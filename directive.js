b0f0343d992d60ac567a1263dcc1d2349bd2e3ff
Fernando Lafuente saiz <fernandolafuentesaiz@gmail.com>
2016-06-30 15:58:37 +0200

    Enable scroll.

--- directive.js
+++ directive.js
@@ -7,7 +7,7 @@
       link: function(scope, element, attrs){
           element.on('load', function(){
               element.parent().find("div.embedLoading").remove(); // Using remove to prevent spinner to overload
-              iFrameResize({log:true});
+              iFrameResize({log:true, scrolling:true});
               return scope.callBack();
           })
       }
